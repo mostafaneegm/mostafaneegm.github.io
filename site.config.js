6183091734ac0d439f974c89dfa0b25f98e58ee1
Mostafa Negm <64106403+mostafaneegm@users.noreply.github.com>
2023-11-30 01:44:09 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "mostafa negm",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Cybersecurity Specialist",
+    bio: "Cybersecurity enthuist",
+    email: "mostafaneegm@gmail.com",
+    linkedin: "mostafaneegm",
+    github: "mostafaneegm",
   },
-  projects: [
+  /*projects: [
     {
       name: `morethan-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
-  ],
+  ],*/
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "mostafa-log",
+    description: "welcome to mostafa-log!",
   },
 
   // CONFIG configration (required)
